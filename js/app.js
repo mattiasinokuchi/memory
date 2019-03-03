c937d9cf29e2fb11007c1ec92df446ad17da368b
mattiasinokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2019-03-03 09:42:50 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,26 +1,23 @@
-// cards array holds all cards
+// declaring array for all cards
 let card = document.getElementsByClassName("card");
 let cards = [...card]
 
-// deck of all cards in game
+// declaring constant for deck of all cards in game
 const deck = document.getElementById("card-deck");
 
-// declaring move variable
+// declaring variable for card moves
 let moves = 0;
 let counter = document.querySelector(".moves");
 
-// declaring variable of matchedCards
+// declaring variable for matched cards
 let matchedCard = document.getElementsByClassName("match");
 
-// array for opened cards
+// declaring array for opened cards
 var openedCards = [];
 
-// @description shuffles cards
-// @param {array}
-// @returns shuffledarray
+// function for shuffle cards
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -28,18 +25,14 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
-};
+}
 
-
-// @description shuffles cards when page is refreshed / loads
+// shuffles cards when page is refreshed or loads
 document.body.onload = startGame();
 
-
-// @description function to start a new play 
+// function to start a new play 
 function startGame(){
-    // shuffle deck
     cards = shuffle(cards);
     // remove all exisiting classes from each card
     for (var i = 0; i < cards.length; i++){
